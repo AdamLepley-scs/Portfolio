32bbf484c6b987ad001e9b12309dbd56e1a6a92b
AdamLepley-scs <adam.lepley@netmatters-scs.com>
2024-08-21 08:58:36 +0100

    form and menu js

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+// mobile nav js
+
+const burgerIcon = document.querySelector('#hamburger');
+    burgerIcon.addEventListener('click', function() {
+
+        const nav = document.querySelector('#sidenav');
+        const navWrapper = document.querySelector('#sidenav-wrapper');
+        const navMenu = document.querySelector('#sidenav-menu');
+        const navSocials = document.querySelector('#sidenav-socials');
+
+        nav.classList.toggle('mobilenav');
+        nav.classList.toggle('sidenav');
+        navWrapper.classList.toggle('mobilenav-wrapper');
+        navWrapper.classList.toggle('sidenav-wrapper');
+        navMenu.classList.toggle('mobilenav-menu');
+        navMenu.classList.toggle('sidenav-menu');
+        navSocials.classList.toggle('mobilenav-socials');
+        navSocials.classList.toggle('sidenav-socials');
+    
+    });
+
+
+
+
+function displayMobileNav(){
+
+
+}
+
+// contact form js
+
+
+
+function submitContactForm(){
+    
+    
+    const conFirst = $("#contact-firstname")
+    const conLast = $("#contact-lastname")
+    const conEmail = $("#contact-email")
+    const conPhone = $("#contact-phone")
+    const conSubject = $("#contact-subject")
+    const conMessage = $("#contact-message")
+
+    const conFields = [conFirst.val(),conLast.val(),conEmail.val(),conPhone.val(),conSubject.val(),conMessage.val()];
+
+    if (conFields.includes('')) {
+        console.log('There is a blank field!')
+        alert('Please fill out all the required fields!');
+    } else {
+        console.log('No fields are blank!')
+    }
+
+}
+     
+    
